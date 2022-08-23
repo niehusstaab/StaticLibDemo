1da4f36a61ea9c4042ddf7c94f133ebad9e2d38f
honey-mobile-release <lnieuhusstaab@paypal.com>
2022-08-23 17:00:19 -0600

    basic package and proj setup

---
+++ Sources/StaticLibDemo/StaticLibDemo.swift
@@ -0,0 +1,6 @@
+public struct StaticLibDemo {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/StaticLibDemoTests/StaticLibDemoTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import StaticLibDemo
+
+final class StaticLibDemoTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(StaticLibDemo().text, "Hello, World!")
+    }
+}
